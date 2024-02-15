301893e0502e841fd0647a686ef84feefdd7a367
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2024-02-15 14:34:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
 
+from setuptools import setup
+
+setup(
+    name='llamaLib',
+    url='https://github.com/JohnNesbit/lag-llama/',
+    author='John Nesbit',
+    author_email='johnmnesbit@gmail.com',
+    packages=['llamaLib'],
+    install_requires=['numpy'],
+    version='0.1',
+    license='MIT',
+    description='LagLlamma packaged',
+)
