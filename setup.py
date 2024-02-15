0508af09ae69f36335639b4a6c6c46985c90c073
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2024-02-15 15:50:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/JohnNesbit/lag-llama/',
     author='John Nesbit',
     author_email='johnmnesbit@gmail.com',
-    packages=['llamaLib'],
+    packages=['llamaLib', 'llamaLib.data','llamaLib.data.augmentations','llamaLib.gluon_utils.gluon_ts_distributions', 'llamaLib.gluon_utils.scalers', 'llamaLib.lag_llama.gluon', 'llamaLib.lag_llama.model', 'llamaLib.gluon_utils', 'llamaLib.lag_llama'],
     install_requires=['numpy'],
     version='0.1',
     license='MIT',
