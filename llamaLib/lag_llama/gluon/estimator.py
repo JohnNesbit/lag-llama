040ed39b62b6f303d3c334e4673cf0d6f96a9210
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2024-02-15 19:52:34 -0500

    Update estimator.py

--- llamaLib/lag_llama/gluon/estimator.py
+++ llamaLib/lag_llama/gluon/estimator.py
@@ -29,11 +29,11 @@ from gluonts.transform import (
 )
 
 from gluonts.torch.distributions import StudentTOutput
-from llamaLib import (
+from llamaLib.gluon_utils.gluon_ts_distributions.implicit_quantile_network import (
     ImplicitQuantileNetworkOutput,
 )
 
-from llamaLib import LagLlamaLightningModule
+from llamaLib.lag_llama.gluon.lightning_module import LagLlamaLightningModule
 
 PREDICTION_INPUT_NAMES = [
     "past_target",
