70bab5ab036f0f57efa2c79e2944a5cef7ac19de
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2024-02-20 11:26:50 -0500

    Update lightning_module.py

--- llamaLib/lightning_module.py
+++ llamaLib/lightning_module.py
@@ -4,7 +4,89 @@ import pytorch_lightning as pl
 import torch
 
 from gluonts.core.component import validated
-from gluonts.torch.modules.loss import DistributionLoss, NegativeLogLikelihood
+
+
+import torch
+from pydantic import BaseModel
+
+
+class DistributionLoss(BaseModel):
+    """
+    A ``torch.nn.Module`` extensions that computes loss values by comparing a
+    ``Distribution`` (prediction) to a ``Tensor`` (ground-truth).
+    """
+
+    def __call__(
+        self, input: torch.distributions.Distribution, target: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Compute the loss of predicting ``target`` with the ``input``
+        distribution.
+
+        Parameters
+        ----------
+        input
+            Distribution object representing the prediction.
+        target
+            Tensor containing the ground truth.
+
+        Returns
+        -------
+        torch.Tensor
+            Tensor containing loss values, with the same shape as ``target``.
+
+        Raises
+        ------
+        NotImplementedError
+            [description]
+        """
+        raise NotImplementedError
+
+
+class NegativeLogLikelihood(DistributionLoss):
+    """
+    Compute the negative log likelihood loss.
+
+    Parameters
+    ----------
+    beta: float in range (0, 1)
+        beta parameter from the paper: "On the Pitfalls of Heteroscedastic
+        Uncertainty Estimation with Probabilistic Neural Networks" by
+        Seitzer et al. 2022
+        https://openreview.net/forum?id=aPOpXlnV1T
+    """
+
+    beta: float = 0.0
+
+    def __call__(
+        self, input: torch.distributions.Distribution, target: torch.Tensor
+    ) -> torch.Tensor:
+        nll = -input.log_prob(target)
+        if self.beta > 0.0:
+            variance = input.variance
+            nll = nll * (variance.detach() ** self.beta)
+        return nll
+
+
+class CRPS(DistributionLoss):
+    def __call__(
+        self, input: torch.distributions.Distribution, target: torch.Tensor
+    ) -> torch.Tensor:
+        return input.crps(target)
+
+
+class QuantileLoss(DistributionLoss):
+    def __call__(
+        self, input: torch.distributions.Distribution, target: torch.Tensor
+    ) -> torch.Tensor:
+        return input.quantile_loss(target)
+
+
+class EnergyScore(DistributionLoss):
+    def __call__(self, input, target: torch.Tensor) -> torch.Tensor:
+        return input.energy_score(target)
+
+
 from gluonts.torch.util import take_last, repeat_along_dim
 from gluonts.itertools import prod
 
